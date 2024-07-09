6120a11c4118c1f07385edc530872dc09acc6fc2
Rishikesh-M <73765860+Rishikesh-M@users.noreply.github.com>
2024-07-09 11:40:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("QuickUp").onclick = function() {
+    window.location.href = "#home";
+  };
+  document.getElementById("Hoodies").onclick = function() {
+    window.location.href = "#hoodie"
+  };
+  document.getElementById("Coffee").onclick = function() {
+    window.location.href = "#coffee";
+  };
+  document.getElementById("Gadgets").onclick = function() {
+    window.location.href = "#gadgets";
+  };
+  document.getElementById("Programs").onclick = function() {
+    window.location.href = "#programs"
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("body").style.display = "block";
+});
